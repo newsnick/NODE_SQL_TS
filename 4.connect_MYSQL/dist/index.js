28c8e98d179a975d39b7d7187c1af58d629e5971
newsnick <atlcontact8@gmail.com>
2024-03-18 17:10:14 +0100

    connect MySQL DB

--- 4.connect_MYSQL/dist/index.js
+++ 4.connect_MYSQL/dist/index.js
@@ -3,16 +3,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
+const mysql2_1 = __importDefault(require("mysql2"));
+const mysqlPassword = process.env.MYSQL_DB_PASSWORD || '';
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(express_1.default.json());
-app.get('/Id/:id/Name/:name', (req, res) => {
-    res.send({
-        message: 'Hello World!',
-        id: req.params.id,
-        name: req.params.name,
+app.get('/details/:id', (req, res) => {
+    var pool = mysql2_1.default.createPool({
+        host: '127.0.0.1',
+        user: 'root',
+        password: mysqlPassword,
+        database: 'beispiel-datenbank',
+        connectionLimit: 10, // Maximale Anzahl von Verbindungen, bevor der Pool auf eine Freigabe wartet
+        multipleStatements: true,
     });
+    pool.getConnection(function (err, conn) {
+        if (err) {
+            // console.log(err);
+            // express.send(400)
+            console.log('Fehler aufgetreten', err);
+            res.send({
+                success: false,
+                statusCode: 500,
+                message: 'Fehler bei der Verbindung',
+            });
+            return;
+        }
+        console.log('Die ID: ' + req.params.id);
+        // Wenn eine Verbindung hergestellt wurde...
+        conn.query('SELECT * FROM kunden WHERE Position=?', [req.params.id], function (err, rows) {
+            if (err) {
+                conn.release();
+                return res.send({
+                    success: false,
+                    statusCode: 400,
+                });
+            }
+            // Zur Vereinfachung senden wir nur diese Zeilen
+            res.send({
+                message: 'Erfolg',
+                statsCode: 200,
+                data: rows,
+            });
+            // VERBINDUNG BEENDEN
+            conn.release();
+        });
+    });
+    // res.send({
+    //   message: 'Hello World!',
+    //   id: req.params.id,
+    //   name: req.params.name,
+    // })
 });
 app.post('/Id/:id/Name/:name', (req, res) => {
     res.send({
@@ -24,5 +68,5 @@ app.post('/Id/:id/Name/:name', (req, res) => {
     });
 });
 app.listen(3000, () => {
-    console.log('The application is listening on port 3000!');
+    console.log('Mit Port 3000 verbunden!');
 });
--- 4.connect_MYSQL/src/index.ts
+++ 4.connect_MYSQL/src/index.ts
@@ -1,16 +1,73 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import { Request, Response } from 'express'
+import mysql from 'mysql2'
+
+const mysqlPassword = process.env.MYSQL_DB_PASSWORD || ''
 
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/Id/:id/Name/:name', (req: Request, res: Response) => {
-  res.send({
-    message: 'Hello World!',
-    id: req.params.id,
-    name: req.params.name,
+app.get('/details/:id', (req: Request, res: Response) => {
+  var pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: mysqlPassword,
+    database: 'beispiel-datenbank',
+    connectionLimit: 10, // Maximale Anzahl von Verbindungen, bevor der Pool auf eine Freigabe wartet
+    multipleStatements: true,
   })
+
+  pool.getConnection(function (err: any, conn: any) {
+    if (err) {
+      // console.log(err);
+      // express.send(400)
+      console.log('Fehler aufgetreten', err)
+      res.send({
+        success: false,
+        statusCode: 500,
+        message: 'Fehler bei der Verbindung',
+      })
+
+      return
+    }
+
+    console.log('Die ID: ' + req.params.id)
+
+    // Wenn eine Verbindung hergestellt wurde...
+    conn.query(
+      'SELECT * FROM kunden WHERE Position=?',
+      [req.params.id],
+      function (err: any, rows: any) {
+        if (err) {
+          conn.release()
+          return res.send({
+            success: false,
+            statusCode: 400,
+          })
+        }
+
+        // Zur Vereinfachung senden wir nur diese Zeilen
+        res.send({
+          message: 'Erfolg',
+          statsCode: 200,
+          data: rows,
+        })
+
+        // VERBINDUNG BEENDEN
+        conn.release()
+      }
+    )
+  })
+
+  // res.send({
+  //   message: 'Hello World!',
+  //   id: req.params.id,
+  //   name: req.params.name,
+  // })
 })
 
 app.post('/Id/:id/Name/:name', (req: Request, res: Response) => {
@@ -24,5 +81,5 @@ app.post('/Id/:id/Name/:name', (req: Request, res: Response) => {
 })
 
 app.listen(3000, () => {
-  console.log('The application is listening on port 3000!')
+  console.log('Mit Port 3000 verbunden!')
 })
